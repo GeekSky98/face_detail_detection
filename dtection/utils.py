3bb18e317e612e43ccdb1b66dac0641beaa89b0e
Deepsky <deepskyai1215@gmail.com>
2023-03-02 23:37:38 +0900

    Reset project & use tensorflow object detection api.

---
+++ dtection/utils.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+from six import BytesIO
+from PIL import Image
+
+import tensorflow as tf
+import tarfile
+import os
+
+from object_detection.utils import label_map_util
+from object_detection.utils import config_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+
+
+def load_image_into_numpy_array(path):
+    img_data = tf.io.gfile.GFile(path, 'rb').read()
+    image = Image.open(BytesIO(img_data))
+    (im_width, im_height) = image.size
+
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+
+def get_model_detection_function(model):
+  def detect_fn(image):
+    image, shapes = model.preprocess(image)
+    prediction_dict = model.predict(image, shapes)
+    detections = model.postprocess(prediction_dict, shapes)
+
+    return detections, prediction_dict, tf.reshape(shapes, [-1])
+
+  return detect_fn
